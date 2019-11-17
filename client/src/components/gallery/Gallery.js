55ab74c29c7d0e4f2d672d1ab71be68beb4523e9
Juz3 <jussi.koivumaki@outlook.com>
2019-11-17 02:34:40 +0200

    added full pictures and select

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -13,16 +13,32 @@ import ImageO4 from "../../res/images/jkpf_thumbs_v1/o4t.jpg";
 import ImageO5 from "../../res/images/jkpf_thumbs_v1/o5t.jpg";
 import ImageO6 from "../../res/images/jkpf_thumbs_v1/o6t.jpg";
 
-import ImageO1Full from "../../res/images/jkpf_thumbs_v1/o1.jpg";
+import pictures from "./pictures";
+
+/* import ImageO1Full from "../../res/images/watermarked/o1w.jpg";
+import ImageO2Full from "../../res/images/watermarked/o2w.jpg";
+import ImageO3Full from "../../res/images/watermarked/o3w.jpg";
+import ImageO4Full from "../../res/images/watermarked/o4w.jpg";
+import ImageO5Full from "../../res/images/watermarked/o5w.jpg";
+import ImageO6Full from "../../res/images/watermarked/o6w.jpg";
+import ImageV1Full from "../../res/images/watermarked/v1w.jpg";
+import ImageV2Full from "../../res/images/watermarked/v2w.jpg";
+import ImageV31Full from "../../res/images/watermarked/v3-1w.jpg";
+import ImageV32Full from "../../res/images/watermarked/v3-2w.jpg";
+import ImageV4Full from "../../res/images/watermarked/v4w.jpg";
+import ImageV5Full from "../../res/images/watermarked/v5w.jpg";
+import ImageV6Full from "../../res/images/watermarked/v6w.jpg"; */
 
 const Gallery = () => {
   const [galleryItems, setGalleryItems] = useState([null]);
 
+  const [picSelect, setPicSelect] = useState(0);
+
   const [modalStyle, setModalStyle] = useState({ display: "none" });
 
-  useEffect(() => {
+  /* useEffect(() => {
     fillArray();
-  }, []);
+  }, []); */
 
   const fillArray = () => {
     const arraySize = 44;
@@ -34,15 +50,37 @@ const Gallery = () => {
     setGalleryItems(numArray);
   };
 
-  const openModal = () => {
+  const openModal = selectedPic => {
     console.log("open modal");
+
+    setPicSelect(selectedPic);
 
     if (modalStyle.display === "none") {
       setModalStyle({ display: "block" });
-      console.log("adsasd");
     } else {
-      console.log(modalStyle);
       setModalStyle({ display: "none" });
+    }
+  };
+
+  const openFullPics = () => {
+    if (picSelect === 0) {
+      return pictures[0];
+    } else {
+      return pictures[picSelect];
+    }
+  };
+
+  const fullPicClick = e => {
+    e.stopPropagation();
+  };
+
+  const changePic = (e, prevOrNext) => {
+    e.stopPropagation();
+
+    if (prevOrNext === false) {
+      setPicSelect(picSelect - 1);
+    } else {
+      setPicSelect(picSelect + 1);
     }
   };
 
@@ -58,33 +96,110 @@ const Gallery = () => {
         {" "}
         <p className="galleryParagraph">Pictures from Finland</p>
       </div>
-      <div className="pictureModal" style={modalStyle}>
-        <img
-          className="fullImg"
-          src={ImageO1Full}
-          alt=""
-          style={{ width: "50%" }}
-        ></img>
+      <div className="pictureModal" style={modalStyle} onClick={openModal}>
+        <div className="modalContent">
+          <button className="closeBtn cursor" onClick={openModal}>
+            Close
+          </button>
+          <button className="prevBtn" onClick={e => changePic(e, false)}>
+            Prev
+          </button>
+          <img
+            className="fullImg"
+            src={openFullPics()}
+            alt=""
+            style={{ width: "50%" }}
+            onClick={e => fullPicClick(e)}
+          ></img>
+          <button className="nextBtn" onClick={e => changePic(e, true)}>
+            Next
+          </button>
+        </div>
       </div>
       <div className="galleryContainer">
         <div> </div>
         <div className="row">
           <div className="column">
-            <img src={ImageV1} alt=""></img>
-            <img src={ImageV2} alt=""></img>
-            <img src={ImageV31} alt=""></img>
-            <img src={ImageV32} alt=""></img>
-            <img src={ImageV4} alt=""></img>
-            <img src={ImageV5} alt=""></img>
-            <img src={ImageV6} alt=""></img>
+            <img
+              className="pictureWall"
+              src={ImageV1}
+              alt=""
+              onClick={() => openModal(6)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageV2}
+              alt=""
+              onClick={() => openModal(7)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageV31}
+              alt=""
+              onClick={() => openModal(8)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageV32}
+              alt=""
+              onClick={() => openModal(9)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageV4}
+              alt=""
+              onClick={() => openModal(10)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageV5}
+              alt=""
+              onClick={() => openModal(11)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageV6}
+              alt=""
+              onClick={() => openModal(12)}
+            ></img>
           </div>
           <div className="column">
-            <img src={ImageO1} alt="" onClick={openModal}></img>
-            <img src={ImageO2} alt=""></img>
-            <img src={ImageO3} alt=""></img>
-            <img src={ImageO4} alt=""></img>
-            <img src={ImageO5} alt=""></img>
-            <img src={ImageO6} alt=""></img>
+            <img
+              className="pictureWall"
+              src={ImageO1}
+              alt=""
+              onClick={() => openModal(0)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageO2}
+              alt=""
+              onClick={() => openModal(1)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageO3}
+              alt=""
+              onClick={() => openModal(2)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageO4}
+              alt=""
+              onClick={() => openModal(3)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageO5}
+              alt=""
+              onClick={() => openModal(4)}
+            ></img>
+            <img
+              className="pictureWall"
+              src={ImageO6}
+              alt=""
+              onClick={() => openModal(5)}
+            ></img>
           </div>
         </div>
         <div> </div>
---
+++ client/src/components/gallery/pictures.js
@@ -0,0 +1,31 @@
+import ImageO1Full from "../../res/images/watermarked/o1w.jpg";
+import ImageO2Full from "../../res/images/watermarked/o2w.jpg";
+import ImageO3Full from "../../res/images/watermarked/o3w.jpg";
+import ImageO4Full from "../../res/images/watermarked/o4w.jpg";
+import ImageO5Full from "../../res/images/watermarked/o5w.jpg";
+import ImageO6Full from "../../res/images/watermarked/o6w.jpg";
+import ImageV1Full from "../../res/images/watermarked/v1w.jpg";
+import ImageV2Full from "../../res/images/watermarked/v2w.jpg";
+import ImageV31Full from "../../res/images/watermarked/v3-1w.jpg";
+import ImageV32Full from "../../res/images/watermarked/v3-2w.jpg";
+import ImageV4Full from "../../res/images/watermarked/v4w.jpg";
+import ImageV5Full from "../../res/images/watermarked/v5w.jpg";
+import ImageV6Full from "../../res/images/watermarked/v6w.jpg";
+
+const pictures = [
+  ImageO1Full,
+  ImageO2Full,
+  ImageO3Full,
+  ImageO4Full,
+  ImageO5Full,
+  ImageO6Full,
+  ImageV1Full,
+  ImageV2Full,
+  ImageV31Full,
+  ImageV32Full,
+  ImageV4Full,
+  ImageV5Full,
+  ImageV6Full
+];
+
+export default pictures;
